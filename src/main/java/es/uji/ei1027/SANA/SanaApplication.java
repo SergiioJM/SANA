b837846c4ff7a25e0fdb4749cb413ffed0fb0b78
SergiioJM <sergiojimenez10@hotmail.com>
2021-04-07 11:22:46 +0200

    Initial commit

---
+++ src/main/java/es/uji/ei1027/SANA/SanaApplication.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.SANA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uji/ei1027/SANA/SanaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uji.ei1027.SANA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
