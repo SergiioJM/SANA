bef83cea2ddc3161208eb7f6aa7afee47731cd8b
Daxito98 <daxyt98@gmail.com>
2021-04-08 11:19:21 +0200

    ez pesy

--- src/main/java/es/uji/ei1027/SANA/controller/MunicipioController.java
+++ src/main/java/es/uji/ei1027/SANA/controller/MunicipioController.java
@@ -1,10 +1,13 @@
 package es.uji.ei1027.SANA.controller;
 
 import es.uji.ei1027.SANA.dao.MunicipioDAO;
+import es.uji.ei1027.SANA.model.Municipio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/municipio")
