a72c55c36ed2321d7640f7e8d96011abdfbc8151
Manel <manelpobla11122000@hotmail.com>
2021-04-13 19:00:52 +0200

    Correciones Periodo

--- src/main/java/es/uji/ei1027/SANA/dao/PeriodoRowMapper.java
+++ src/main/java/es/uji/ei1027/SANA/dao/PeriodoRowMapper.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class PeriodoRowMapper implements RowMapper<Periodo> {
+public final class PeriodoRowMapper implements RowMapper<Periodo> {
     public Periodo mapRow(ResultSet rs, int rowNum) throws SQLException {
         Periodo periodo = new Periodo();
         periodo.setIdentificador(rs.getString("identificador"));
