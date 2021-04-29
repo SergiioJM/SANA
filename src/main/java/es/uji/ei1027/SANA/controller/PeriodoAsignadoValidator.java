a1ed24337152d2608d66d0368aaee01720d2be21
SergiioJM <sergiojimenez10@hotmail.com>
2021-04-29 12:04:35 +0200

    ?

--- src/main/java/es/uji/ei1027/SANA/controller/PeriodoAsignadoValidator.java
+++ src/main/java/es/uji/ei1027/SANA/controller/PeriodoAsignadoValidator.java
@@ -1,8 +1,13 @@
 package es.uji.ei1027.SANA.controller;
 
+import es.uji.ei1027.SANA.dao.ControladorDAO;
+import es.uji.ei1027.SANA.model.Controlador;
 import es.uji.ei1027.SANA.model.PeriodoAsignado;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PeriodoAsignadoValidator implements Validator {
     @Override
@@ -13,6 +18,7 @@ public class PeriodoAsignadoValidator implements Validator {
     @Override
     public void validate(Object obj, Errors errors) {
         PeriodoAsignado periodoAsignado = (PeriodoAsignado) obj;
+        ControladorDAO controladorDAO = new ControladorDAO();
         if (periodoAsignado.getIdentificador().trim().equals(""))
             errors.rejectValue("identificador","obligatorio", "El periodo asignado tiene que tener un identificador");
 
@@ -22,6 +28,12 @@ public class PeriodoAsignadoValidator implements Validator {
         if (periodoAsignado.getArea().trim().equals(""))
             errors.rejectValue("area","obligatorio", "El periodo asignado debe tener un area");
 
-    }
+        List<String> controladores = new ArrayList<>();
+        for (Controlador c: controladorDAO.getControladores()){
+            controladores.add(c.getNombre());
+        }
+        if (!controladores.contains(periodoAsignado.getControlador()))
+            errors.rejectValue("controlador","obligatorio","El controlador debe de existir");
 
+    }
 }
